5967c6f4194de586219e9b2a217dbfb15bc4f272
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-08 14:21:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+
+export default {
+  components: {
+    AppHeader,
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>

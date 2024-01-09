500115f4887e966c5db49b5043d3f6dfcec2e840
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-01-09 15:50:02 +0100

    Option della select popolate dinamicamente chiamando l'API in base all' "archetype"

--- src/components/SearchType.vue
+++ src/components/SearchType.vue
@@ -1,17 +1,41 @@
 <script>
+//Importo lo store
+import { store } from '../store';
+
+//Importo axios
+import axios from 'axios';
 
 export default {
-    name: 'SearchType'
+    name: 'SearchType',
+    data() {
+        return {
+            store
+        }
+    },
+    methods: {
+        getArchetype() {
+            axios.get(store.apiGetType)
+                .then((res => {
+                    store.cardType = res.data;
+                }))
+                .catch((err => {
+                    console.log("Questi sono gli errori", err);
+                }));
+        }
+    },
+    created() {
+        this.getArchetype();
+        console.log(this.getArchetype);
+    }
 }
 
 </script>
 
 <template>
     <select class="bg-white border border-dark rounded-2 my-3 py-1 px-4" name="type" id="">
-        <option value="volvo">Alien</option>
-        <option value="saab">Human</option>
-        <option value="opel">Robot</option>
-        <option value="audi">idk</option>
+        <option value="">Select archetype</option>
+        <option v-for="(archetype, index) in store.cardType" :key="index" value="archetype">{{ archetype.archetype_name }}
+        </option>
     </select>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -4,4 +4,6 @@ export const store = reactive({
     CardsList: [],
     apiURL: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
     loading: true,
+    cardType: [],
+    apiGetType: 'https://db.ygoprodeck.com/api/v7/archetypes.php',
 });
